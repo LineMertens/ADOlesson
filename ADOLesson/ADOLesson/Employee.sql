0e61e207dc57ba0b53edd3d071f3dd2d9d667ae2
LineMertens <line.mertens@gmail.com>
2019-07-01 13:53:18 +0200

    DB init
    Initialisation de la DB via un project SQLServer

---
+++ ADOLesson/ADOLesson/Employee.sql
@@ -0,0 +1,28 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL  IDENTITY,
+	[Firstname] NVARCHAR(50) NOT NULL, 
+	[LastName] VARCHAR (50) NOT NULL,
+	[BirthDate] DATETIME2 NOT NULL, 
+    [Email] NVARCHAR(100) NOT NULL UNIQUE, 
+    [ResponsableID] INT NULL, 
+    CONSTRAINT [PK_Employee] PRIMARY KEY ([Id]), 
+    CONSTRAINT [AK_Employee_email] UNIQUE ([Email]), 
+    CONSTRAINT [CK_Employee_Birthdate] CHECK (YEAR(BirthDate) < 2010), 
+    CONSTRAINT [CK_Employee_Reponsableid] CHECK (ResponsableID <> Id), 
+    CONSTRAINT [FK_Employee_Employee] FOREIGN KEY ([ResponsableID]) REFERENCES [Employee]([ID]) 
+)
+
+GO
+
+CREATE INDEX [IX_Employee_LastName] ON [dbo].[Employee] ([LastName])
+
+GO
+
+CREATE TRIGGER [dbo].[Trigger_OnInsertUpdateEmployee]
+    ON [dbo].[Employee]
+    FOR DELETE, INSERT, UPDATE
+    AS
+    BEGIN
+        SET NoCount ON
+    END
---
+++ ADOLesson/ADOLesson/V_employee.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW [dbo].[V_employee]
+	AS SELECT 
+	Id, LastName, Firstname, BirthDate, Email, ResponsableID, 
+	M.LastName AS RespLastName, 
+	M.Firstname AS RespFirstName, 
+	M.Email AS RespEmail
+	FROM [Employee] E JOIN [Employee] M ON E.ResponsableID = M.Id 
